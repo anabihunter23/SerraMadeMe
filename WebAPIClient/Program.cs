87602c18ba005133123d59b182c4ff35e8b59acd
Aqib <54675771+anabihunter23@users.noreply.github.com>
2022-02-27 19:59:35 -0500

    Add files via upload
    Keep getting namespace cannot directly contain members such as as fields or methods (CS0116)

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WebAPIClient
+{
+    class Pokémon
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("weight")]
+        public int Weight { get; set; }
+
+        [JsonProperty("height")]
+        public int Height { get; set; }
+
+        public List<Types> Types { get; set; }
+    }
+
+    public class Type
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class Types
+    {
+        [JsonProperty("type")]
+        public Type Type;
+    }
+
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter Pokémon name. Press Enter without writing a name to quit the program");
+
+                    var pokémonName = Console.ReadLine();
+                    if (string.IsNullOrEmpty(pokémonName))
+                    {
+                        break;
+                    }
+
+                    var result = await client.GetAsync("https://pokeapi.co/api/v2/pokémon" + pokémonName.ToLower());
+                    var resultRead = await result.Content.ReadAsStringAsync();
+                    var pokémon = JsonConvert.DeserializeObject<Pokémon>(resultRead);
+
+                    Console.WriteLine("---");
+                    Console.WriteLine("Pokémon #" + pokémon.Id);
+                    Console.WriteLine("Name: " + pokémon.Name);
+                    Console.WriteLine("Weight: " + pokémon.Weight + "lb");
+                    Console.WriteLine("Height: " + pokémon.Height + "ft");
+                    Console.WriteLine("Type(s):");
+                    pokémon.Types.ForEach(Task => Console.Write(" " + t.Type.Name));
+                    Console.WriteLine("\n---");
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("ERROR. Please enter a valid Pokémon name!");
+                }
+            }
+        }
+    }
+}
